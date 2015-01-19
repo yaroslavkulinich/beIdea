ef15404088241ebd72e27864b5120a429effdc92
mtwain <mtwain94@gmail.com>
2015-01-19 17:41:21 +0200

    Додане View відображання ідеї та її редактування

--- app/src/main/java/beIdea/mtwain/besqueet/beidea/ui/fragments/BeIdeaFragment.java
+++ app/src/main/java/beIdea/mtwain/besqueet/beidea/ui/fragments/BeIdeaFragment.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.backup.BackupManager;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -13,20 +14,25 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.getbase.floatingactionbutton.FloatingActionButton;
 import com.getbase.floatingactionbutton.FloatingActionsMenu;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.slidinglayer.SlidingLayer;
 import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 import com.squareup.picasso.Picasso;
 
 import java.io.File;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Calendar;
 
@@ -34,31 +40,41 @@ import beIdea.mtwain.besqueet.beidea.BeIdeaActivity;
 import beIdea.mtwain.besqueet.beidea.Constants;
 import beIdea.mtwain.besqueet.beidea.R;
 import beIdea.mtwain.besqueet.beidea.controllers.RealmController;
+import beIdea.mtwain.besqueet.beidea.ui.Idea;
 import beIdea.mtwain.besqueet.beidea.ui.adapters.ListIdeaAdapter;
 import beIdea.mtwain.besqueet.beidea.utils.Utilites;
+import io.realm.Realm;
+import me.drakeet.materialdialog.MaterialDialog;
 import se.emilsjolander.stickylistheaders.StickyListHeadersListView;
 
 
 public class BeIdeaFragment extends Fragment implements Constants,View.OnClickListener {
 
-    LinearLayout horizontalLinearLayout;
+    LinearLayout generalImageContainer,detailImageContainer;
     SlidingUpPanelLayout slidingUpPanelLayout;
     FloatingActionsMenu actionButton;
-    FloatingActionButton btnTakePhoto,btnGetPicture;
+    FloatingActionButton btnTakePhoto,btnGetPicture,btnEditIdea,btnSaveIdea;
     RelativeLayout dragLayout;
     EditText etIdea,etTitle;
-    SlidingLayer slidingLayer;
-    StickyListHeadersListView stickyList;
+    SlidingLayer showIdeaLayer,saveButtonLayer;
     ListIdeaAdapter listIdeaAdapter;
-    TextView tvDetailIdea,tvDetailDate,tvDetailTime;
+    EditText etDetailIdea, etDetailTitle;
+    ImageView add_detail_image;
+
 
     private static final int SELECT_PHOTO = 1;//TODO: to constants
     private static final int REQUEST_IMAGE_CAPTURE = 2 ;//TODO: to constants
+    private static final int SELECT_PHOTO_DETAIL = 3;//TODO: to constants
+    private static final int REQUEST_IMAGE_CAPTURE_DETAIL = 4 ;//TODO: to constants
 
     static String title,idea,currentPicturePath = "";
     boolean state = false;
+    boolean editingEnable = false;
+    Idea editingIdea;
+    int editingIdeaIndex;
 
     ArrayList<String>imagePaths = new ArrayList<>();
+    ArrayList<String>editingImages = new ArrayList<>();
 
     public BeIdeaFragment(){}
 
@@ -86,7 +102,8 @@ public class BeIdeaFragment extends Fragment implements Constants,View.OnClickLi
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_beidea, container, false);
-        horizontalLinearLayout = (LinearLayout) rootView.findViewById(R.id.horizontalLinearLayout);
+        generalImageContainer = (LinearLayout) rootView.findViewById(R.id.general_image_container);
+        detailImageContainer = (LinearLayout) rootView.findViewById(R.id.detail_image_container);
         actionButton = (FloatingActionsMenu) rootView.findViewById(R.id.actionButton);
         btnTakePhoto = (FloatingActionButton) rootView.findViewById(R.id.takePhoto);
         btnGetPicture = (FloatingActionButton) rootView.findViewById(R.id.getPicture);
@@ -129,58 +146,66 @@ public class BeIdeaFragment extends Fragment implements Constants,View.OnClickLi
                 Log.i("", "onPanelHidden");
             }
         });
-        /*final View rootView = inflater.inflate(R.layout.fragment_list_idea, container, false);
 
-        stickyList = (StickyListHeadersListView) rootView.findViewById(R.id.lvIdea);
-        ideaAdapter = new ListIdeaAdapter(this);
-        slidingLayer = (SlidingLayer) rootView.findViewById(R.id.slidingLayer);
-        tvDetailIdea = (TextView) slidingLayer.findViewById(R.id.tvDetailIdea);
-        tvDetailDate = (TextView) slidingLayer.findViewById(R.id.tvDetailDate);
-        tvDetailTime = (TextView) slidingLayer.findViewById(R.id.tvDetailTime);
+        saveButtonLayer = (SlidingLayer) rootView.findViewById(R.id.saveButtonLayer);
+        saveButtonLayer.setCloseOnTapEnabled(false);
+        saveButtonLayer.setSlidingEnabled(false);
 
-        slidingLayer.setShadowWidthRes(R.dimen.shadow_width);
-        slidingLayer.setOffsetWidth(1);
-        slidingLayer.setCloseOnTapEnabled(true);
-        slidingLayer.setStickTo(SlidingLayer.STICK_TO_LEFT);
-
-        stickyList.setAdapter(ideaAdapter);
-        stickyList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        showIdeaLayer = (SlidingLayer) rootView.findViewById(R.id.slidingLayer);
+        showIdeaLayer.setShadowWidthRes(R.dimen.shadow_width);
+        showIdeaLayer.setOffsetWidth(1);
+        showIdeaLayer.setCloseOnTapEnabled(false);
+        showIdeaLayer.setStickTo(SlidingLayer.STICK_TO_RIGHT);
+        showIdeaLayer.setOnInteractListener(new SlidingLayer.OnInteractListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                slidingLayer.openLayer(true);
-                tvDetailIdea.setText(RealmController.getIdeas().get(i).getIdea());
-                tvDetailDate.setText(RealmController.getIdeas().get(i).getDate());
-                tvDetailTime.setText(RealmController.getIdeas().get(i).getTime());
+            public void onOpened() {
             }
-        });*/
-       /*final View rootView = inflater.inflate(R.layout.fragment_list_idea, container, false);
 
-        stickyList = (StickyListHeadersListView) rootView.findViewById(R.id.lvIdea);
-        ideaAdapter = new ListIdeaAdapter(this);
-        slidingLayer = (SlidingLayer) rootView.findViewById(R.id.slidingLayer);
-        tvDetailIdea = (TextView) slidingLayer.findViewById(R.id.tvDetailIdea);
-        tvDetailDate = (TextView) slidingLayer.findViewById(R.id.tvDetailDate);
-        tvDetailTime = (TextView) slidingLayer.findViewById(R.id.tvDetailTime);
-
-        slidingLayer.setShadowWidthRes(R.dimen.shadow_width);
-        slidingLayer.setOffsetWidth(1);
-        slidingLayer.setCloseOnTapEnabled(true);
-        slidingLayer.setStickTo(SlidingLayer.STICK_TO_LEFT);
-
-        stickyList.setAdapter(ideaAdapter);
-        stickyList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                slidingLayer.openLayer(true);
-                tvDetailIdea.setText(RealmController.getIdeas().get(i).getIdea());
-                tvDetailDate.setText(RealmController.getIdeas().get(i).getDate());
-                tvDetailTime.setText(RealmController.getIdeas().get(i).getTime());
+            public void onOpen() {
+
             }
-        });*/
+            @Override
+            public void onClosed() {
+                editingEnable = false;
+                editingImages = new ArrayList<>();
+                etDetailIdea.setEnabled(false);
+                etDetailTitle.setEnabled(false);
+                showSaveDetailButton(false);
+            }
+            @Override
+            public void onClose() {
+
+            }
+        });
+        etDetailIdea = (EditText) showIdeaLayer.findViewById(R.id.tvDetailIdea);
+        etDetailTitle = (EditText) showIdeaLayer.findViewById(R.id.tvDetailTitle);
+        btnEditIdea = (FloatingActionButton) rootView.findViewById(R.id.editIdea);
+        btnEditIdea.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editDetail();
+            }
+        });
+        btnSaveIdea = (FloatingActionButton) saveButtonLayer.findViewById(R.id.saveIdea);
+        btnSaveIdea.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveDetail();
+
+            }
+        });
 
         StickyListHeadersListView cardListView = (StickyListHeadersListView) rootView.findViewById(R.id.card_idea_list);
         listIdeaAdapter = new ListIdeaAdapter(getActivity());
         cardListView.setAdapter(listIdeaAdapter);
+        cardListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                editingIdeaIndex = i;
+                showIdea(i);
+            }
+        });
         btnGetPicture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -203,9 +228,61 @@ public class BeIdeaFragment extends Fragment implements Constants,View.OnClickLi
                 actionButton.collapse();
             }
         });
+
+        add_detail_image = (ImageView) showIdeaLayer.findViewById(R.id.add_image);
+        add_detail_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
+                        getActivity(),
+                        android.R.layout.simple_list_item_1
+                );
+                arrayAdapter.add("Take photo");
+                arrayAdapter.add("Gallery");
+                ListView listView = new ListView(getActivity());
+                listView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+                float scale = getResources().getDisplayMetrics().density;
+                int dpAsPixels = (int) (8 * scale + 0.5f);
+                listView.setPadding(0, dpAsPixels, 0, dpAsPixels);
+                listView.setDividerHeight(0);
+                listView.setAdapter(arrayAdapter);
+
+                final MaterialDialog alert = new MaterialDialog(getActivity())
+                        .setTitle("Add Image")
+                        .setContentView(listView);
+                alert.setPositiveButton(
+                        "Cancel", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                alert.dismiss();
+                            }
+                        }
+                );
+                alert.show();
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        if(i==0){
+                            Intent makePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                            File image = Utilites.generatePicturePath();
+                            if (image != null) {
+                                makePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(image));
+                                currentPicturePath = image.getAbsolutePath();
+                            }
+                            startActivityForResult(makePhotoIntent, REQUEST_IMAGE_CAPTURE_DETAIL);
+                            alert.dismiss();
+                        }else if(i==1){
+                            Intent imagePickerIntent = new Intent(Intent.ACTION_PICK);
+                            imagePickerIntent.setType("image/*");
+                            startActivityForResult(imagePickerIntent,SELECT_PHOTO_DETAIL);
+                            alert.dismiss();
+                        }
+                    }
+                });
+            }
+        });
         return rootView;
     }
-
 
 
     @Override
@@ -224,8 +301,92 @@ public class BeIdeaFragment extends Fragment implements Constants,View.OnClickLi
                         putImage(currentPicturePath);
                     }
                     break;
+                case REQUEST_IMAGE_CAPTURE_DETAIL:
+                    File fileDetail = new File(currentPicturePath);
+                    Boolean bDetail = fileDetail.exists();
+                    if (bDetail) {
+                        putImageIntoDetail(currentPicturePath,true);
+                    }
+                    break;
+                case SELECT_PHOTO_DETAIL:
+                    Uri selectedImageDetail = data.getData();
+                    String imagePathDetail = Utilites.getPath(getActivity(), selectedImageDetail);
+                    putImageIntoDetail(imagePathDetail,true);
+                    break;
+
             }
         }
+    }
+
+    public void putImageIntoDetail(final String path,boolean once){
+        if(once) {
+            if (editingImages.contains(path)) {
+                return;
+            }
+            editingImages.add(path);
+        }
+        LayoutInflater lInflater = (LayoutInflater) getActivity()
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        final View photoView = lInflater.inflate(R.layout.photo_view, detailImageContainer, false);
+        ImageView image = (ImageView) photoView.findViewById(R.id.imgImage);
+        ImageView imageRemove = (ImageView) photoView.findViewById(R.id.imgRemove);
+        detailImageContainer.addView(photoView);
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment fragment = new GalleryFragment();
+                Bundle bundle = new Bundle();
+                bundle.putString(BUNDLE_PATH, path);
+                bundle.putStringArrayList(BUNDLE_IMAGE_PATHS, editingImages);
+                fragment.setArguments(bundle);
+                ((BeIdeaActivity) getActivity()).presentFragment(fragment);
+            }
+        });
+        imageRemove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editingImages.remove(path);
+                photoView.setVisibility(View.GONE);
+                add_detail_image.setVisibility(View.VISIBLE);
+            }
+        });
+        File file = new File(path);
+        Boolean b = file.exists();
+        if (b){
+            Picasso.with(getActivity())
+                    .load(new File(path))
+                    .resize(50, 50)
+                    .centerCrop()
+                    .placeholder(R.drawable.ic_launcher)//TODO:Додати картинку
+                    .error(R.drawable.ic_launcher)//TODO:Додати картинку помилки
+                    .into(image);
+        }else {
+            Log.d("B","File doesn't exists");
+        }
+        if(!editingEnable){
+            imageRemove.setVisibility(View.GONE);
+            add_detail_image.setVisibility(View.INVISIBLE);
+        }else{
+            add_detail_image.setVisibility(View.VISIBLE);
+        }
+
+        if(editingImages.size()>=3){
+            add_detail_image.setVisibility(View.GONE);
+        }
+
+    }
+
+    public void showSaveDetailButton(boolean show){
+        if(show){
+            saveButtonLayer.openLayer(true);
+            Drawable icon = getResources().getDrawable(R.drawable.ic_action_close);
+            btnEditIdea.setIconDrawable(icon);
+        }else{
+            Drawable icon = getResources().getDrawable(R.drawable.ic_action_edit);
+            btnEditIdea.setIconDrawable(icon);
+            saveButtonLayer.closeLayer(true);
+        }
+
     }
 
     public void putImage(final String path){
@@ -234,10 +395,10 @@ public class BeIdeaFragment extends Fragment implements Constants,View.OnClickLi
         LayoutInflater lInflater = (LayoutInflater) getActivity()
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-        final View photoView = lInflater.inflate(R.layout.photo_view, horizontalLinearLayout, false);
+        final View photoView = lInflater.inflate(R.layout.photo_view, generalImageContainer, false);
         ImageView image = (ImageView) photoView.findViewById(R.id.imgImage);
         ImageView imageRemove = (ImageView) photoView.findViewById(R.id.imgRemove);
-        horizontalLinearLayout.addView(photoView);
+        generalImageContainer.addView(photoView);
 
         image.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -276,6 +437,61 @@ public class BeIdeaFragment extends Fragment implements Constants,View.OnClickLi
             //TODO:додати функцію видалення - в ній зробити кнопку видимою
         }
     }
+
+    public void showIdea(int position){
+        detailImageContainer.removeAllViews();
+        showIdeaLayer.openLayer(true);
+        editingIdea = RealmController.getIdeas().get(position);
+        etDetailIdea.setText(editingIdea.getIdea());
+        etDetailTitle.setText(editingIdea.getTime());
+        Gson gson = new Gson();
+        Type collectionType = new TypeToken<ArrayList<String>>(){}.getType();
+        editingImages = gson.fromJson(editingIdea.getImages(), collectionType);
+        for(int i=0;i<editingImages.size();i++){
+            putImageIntoDetail(editingImages.get(i),false);
+        }
+        if(!editingEnable){
+            add_detail_image.setVisibility(View.GONE);
+        }
+
+    }
+
+
+    public void editDetail(){
+        if(!editingEnable){
+            editingEnable = true;
+            etDetailIdea.setEnabled(true);
+            etDetailTitle.setEnabled(true);
+            showIdea(editingIdeaIndex);
+            etDetailIdea.requestFocus();
+            etDetailIdea.setSelection(etDetailIdea.getText().length(), etDetailIdea.getText().length());
+            showSaveDetailButton(true);
+
+        }else {
+            etDetailIdea.setEnabled(false);
+            etDetailTitle.setEnabled(false);
+            editingEnable = false;
+            showSaveDetailButton(false);
+            showIdea(editingIdeaIndex);
+
+        }
+    }
+
+    public void saveDetail(){
+        Realm realm = Realm.getInstance(getActivity());
+        realm.beginTransaction();
+        editingIdea.setIdea(etDetailIdea.getText() + "");
+        editingIdea.setTitle(etDetailTitle.getText() + "");
+        editingIdea.setImages(new Gson().toJson(editingImages));
+        realm.commitTransaction();
+        saveButtonLayer.closeLayer(true);
+        etDetailIdea.setEnabled(false);
+        etDetailTitle.setEnabled(false);
+        editingEnable = false;
+        showIdeaLayer.closeLayer(true);
+        listIdeaAdapter.notifyDataSetChanged();
+    }
+
 
     @Override
     public void onClick(View view) {
@@ -322,7 +538,7 @@ public class BeIdeaFragment extends Fragment implements Constants,View.OnClickLi
     public void clearFields(){
         etTitle.setText("");
         etIdea.setText("");
-        horizontalLinearLayout.removeAllViews();
+        generalImageContainer.removeAllViews();
         actionButton.setVisibility(View.VISIBLE);
     }
 
